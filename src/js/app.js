1b869b9fb44cc4c02e02da89c5af005334ec1820
Ugo <contact@onaliugo.fr>
2014-08-31 10:10:51 +0200

    JS: Now support slide childs

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ $(function(){
   var Slides = {
     $container: $('.slides-container'),
     $el: $('.slide'),
-    counter: 0,
+    el_counter: 0,
 
     init: function(){
       var slidesLength = this.$el.length;
@@ -29,35 +29,62 @@ $(function(){
       this.updateCurrent();
     },
     updateCurrent: function(){
-      this.$el_current = this.$el.eq(this.counter);
+      this.$el_current = this.$el.eq(this.el_counter);
     },
-    anim: function(dir){
+    animCtrl: function(dir){
+      var self = this;
+      var $currentSlide = this.$el_current;
+      var $currentChild = $currentSlide.find('.slide-child').not('.is-show').first();
+      var hasChild = $currentChild.length > 0;
+
+      if(hasChild && dir === 'next')
+        this.animChild($currentChild);
+      else
+        this.animSlide(dir, $currentSlide);
+    },
+    animChild: function($el, cb){
+      var self = this;
+      var animClass = $el.attr('data-animation');
+
+      $el.addClass(animClass+ ' animated');
+
+      setTimeout(function(){
+        $el.toggleClass('is-show animated '+ animClass);
+        animationRunning = false;
+      }, 600);
+    },
+    animSlide: function(dir){
       var self = this;
       var $currentSlide = this.$el_current;
       var $nextSlide;
 
       if(dir === 'next'){
         $nextSlide = $currentSlide.next('.slide');
-        this.counter++;
+        this.el_counter++;
       }
       else{
         $nextSlide = $currentSlide.prev('.slide');
-        this.counter--;
+        this.el_counter--;
       }
 
       var nextSlideAnimation = $nextSlide.attr('data-animation');
 
       $currentSlide.addClass('fadeOut animated');
-      $nextSlide.addClass(nextSlideAnimation + ' slide--current animated');
+      $nextSlide.addClass(nextSlideAnimation + ' is-current animated');
 
       setTimeout(function(){
-        $currentSlide.removeClass('fadeOut animated slide--current');
+        $currentSlide.removeClass('fadeOut animated is-current')
+          .find('.slide-child.is-show')
+            .removeClass('is-show');
+
         $nextSlide.removeClass(nextSlideAnimation + ' animated');
+
         self.updateCurrent();
         animationRunning = false;
       }, 1000);
     }
   };
+
 
   Slides.init();
 
@@ -69,16 +96,16 @@ $(function(){
 
     if(keyCode === 39){
       dir = 'next';
-      canAnim = (Slides.counter < Slides.slidesLength - 1);
+      canAnim = (Slides.el_counter < Slides.slidesLength - 1);
     }
     else if(keyCode === 37){
       dir = 'prev';
-      canAnim = (Slides.counter > 0);
+      canAnim = (Slides.el_counter > 0);
     }
 
     if(canAnim && !animationRunning){
       animationRunning = true;
-      Slides.anim(dir);
+      Slides.animCtrl(dir);
     }
   });
 });
