03e42e7e4e02fe27285cc1ce11662c753551b7e9
Ugo <contact@onaliugo.fr>
2014-08-31 20:09:21 +0200

    JS and CSS optimizations

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 $(function(){
   var $body = $('body');
   var $doc = $(document);
-
   var animationRunning = false;
   var bodyWidth = $body.width();
 
@@ -14,13 +13,9 @@ $(function(){
       var slidesLength = this.$el.length;
       this.slidesLength = slidesLength;
 
-      // set container size
-      this.$container.width( slidesLength * this.body_width );
-
       // set width & z-index on each slide
       this.$el.each(function(i, el){
         $(el).css({
-          'width': bodyWidth,
           'z-index': (slidesLength - i) * 10
         });
       });
